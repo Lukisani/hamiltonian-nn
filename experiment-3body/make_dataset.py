585a0a27675de11369562a3ed43918264c99d57d
Lukisani <luki.sanitra@gmail.com>
2024-12-28 18:59:32 +0400

    Update make_dataset.py

--- experiment-3body/make_dataset.py
+++ experiment-3body/make_dataset.py
@@ -1,7 +1,9 @@
-import os, to_pickle
+import os, sys
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(PARENT_DIR)
 
-
+from utils import to_pickle
 from data import make_orbits_dataset
 
 path = '{}/{}-orbits-dataset.pkl'.format(THIS_DIR, '3body')
