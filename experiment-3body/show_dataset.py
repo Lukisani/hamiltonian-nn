36e9adb2e1f35df61fc16410a417296ef65f159a
Lukisani <luki.sanitra@gmail.com>
2024-12-28 19:36:32 +0400

    Update show_dataset.py

--- experiment-3body/show_dataset.py
+++ experiment-3body/show_dataset.py
@@ -9,9 +9,13 @@ path = '{}/{}-orbits-dataset.pkl'.format(THIS_DIR, '3body')
 
 data = from_pickle(path)
 print(data)
+print()
 print('coords shape:', data['coords'].shape)
 print('test_coords:', data['test_coords'].shape)
 print('dcoords shape:', data['dcoords'].shape)
 print('test_dcoords shape:', data['test_dcoords'].shape)
 print('energy shape:', data['energy'].shape)
 print('test_energy shape:', data['test_energy'].shape)
+print('meta:')
+for key, value in data['meta'].items():
+    print('\t' + key + ':', value)
