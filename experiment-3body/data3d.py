20ee6edd3675d1287a82fbdb0cb6e3126f3655c1
Lukisani <luki.sanitra@gmail.com>
2025-01-05 00:25:28 +0400

    Update data3d.py

--- experiment-3body/data3d.py
+++ experiment-3body/data3d.py
@@ -125,23 +125,28 @@ def sample_orbits(timesteps=20, trials=5000, nbodies=3, orbit_noise=2e-1,
     x, dx, e = [], [], []
     N = timesteps*trials
     while len(x) < N:
+        try:
 
-        state = random_config(nu=orbit_noise, min_radius=min_radius, max_radius=max_radius)
-        orbit, settings = get_orbit(state, t_points=timesteps, t_span=t_span, nbodies=nbodies, **kwargs)
-        batch = orbit.transpose(2,0,1).reshape(-1,nbodies*7)
+            state = random_config(nu=orbit_noise, min_radius=min_radius, max_radius=max_radius)
+            orbit, settings = get_orbit(state, t_points=timesteps, t_span=t_span, nbodies=nbodies, **kwargs)
+            batch = orbit.transpose(2,0,1).reshape(-1,nbodies*7)
 
-        for state in batch:
-            dstate = update(None, state)
-            
-            # reshape from [nbodies, state] where state=[m, qx, qy, px, py]
-            # to [canonical_coords] = [qx1, qx2, qy1, qy2, px1,px2,....]
-            coords = state.reshape(nbodies,7).T[1:].flatten()
-            dcoords = dstate.reshape(nbodies,7).T[1:].flatten()
-            x.append(coords)
-            dx.append(dcoords)
+            for state in batch:
+                dstate = update(None, state)
+                
+                # reshape from [nbodies, state] where state=[m, qx, qy, px, py]
+                # to [canonical_coords] = [qx1, qx2, qy1, qy2, px1,px2,....]
+                coords = state.reshape(nbodies,7).T[1:].flatten()
+                dcoords = dstate.reshape(nbodies,7).T[1:].flatten()
+                x.append(coords)
+                dx.append(dcoords)
 
-            shaped_state = state.copy().reshape(nbodies,7,1)
-            e.append(total_energy(shaped_state))
+                shaped_state = state.copy().reshape(nbodies,7,1)
+                e.append(total_energy(shaped_state))
+        
+        except Exception as e:
+            print(f"Error during orbit sampling: {e}")
+            break
 
     data = {'coords': np.stack(x)[:N],
             'dcoords': np.stack(dx)[:N],
@@ -169,7 +174,7 @@ def get_dataset(experiment_name, save_dir, **kwargs):
     '''Returns an orbital dataset. Also constructs
     the dataset if no saved version is available.'''
 
-    path = '{}/{}-orbits-dataset.pkl'.format(save_dir, experiment_name)
+    path = '{}/{}-3Dorbits-dataset.pkl'.format(save_dir, experiment_name)
 
     try:
         data = from_pickle(path)
