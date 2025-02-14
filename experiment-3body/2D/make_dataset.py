29864da604a65bc99baed5ea327c5dc1f73f5bf0
Lukisani <luki.sanitra@gmail.com>
2025-02-15 03:12:18 +0400

    Update make_dataset.py

--- experiment-3body/2D/make_dataset.py
+++ experiment-3body/2D/make_dataset.py
@@ -2,17 +2,16 @@
 
 import os, sys
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print('Parentttttttttt:', PARENT_DIR)
-# PARENT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PARENT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(PARENT_DIR)
 
-# from utils import to_pickle
-# from data import make_orbits_dataset
+from utils import to_pickle
+from data import make_orbits_dataset
 
 
-# path = '{}/{}-orbits-dataset.pkl'.format(THIS_DIR, '3body')
-# data = make_orbits_dataset(verbose=True)
-# print('Dataset created successfully')
-# to_pickle(data, path)
-# print('...saved in', path)
+path = '{}/{}-orbits-dataset.pkl'.format(THIS_DIR, '3body')
+data = make_orbits_dataset(verbose=True)
+print('Dataset created successfully')
+to_pickle(data, path)
+print('...saved in', path)
