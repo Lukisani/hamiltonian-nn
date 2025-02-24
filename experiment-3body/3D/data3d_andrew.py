9e2416af336b367d9f2d31de6870c965f0338710
Lukisani <luki.sanitra@gmail.com>
2025-02-24 21:42:48 +0400

    Update data3d_andrew.py

--- experiment-3body/3D/data3d_andrew.py
+++ experiment-3body/3D/data3d_andrew.py
@@ -157,9 +157,9 @@ def worker_func(args, orbit_kwargs):
     return simulate_orbit(*args, **orbit_kwargs)
 
 ##### PARALLEL ORBIT SAMPLING WITH PROGRESS BAR #####
-def sample_orbits_parallel(timesteps=20, trials=5000, nbodies=3, orbit_noise=2e-1,
+def sample_orbits_parallel(timesteps=20, trials=20000, nbodies=3, orbit_noise=2e-1,
                            min_radius=0.9, max_radius=1.2, t_span=[0, 5],
-                           nprocs=2*mp.cpu_count(), **kwargs):
+                           nprocs=2*mp.cpu_count(), **kwargs): # trials were at 5000 originally
     """
     Run orbit simulations in parallel using multiprocessing.
     Each process simulates one orbit (which gives several time steps/samples).
