5551e21eca4970cb2a43866535c359ce4edd4152
Lukisani <luki.sanitra@gmail.com>
2025-03-01 17:35:05 +0400

    Update show_3d_dataset.py

--- experiment-3body/3D/show_3d_dataset.py
+++ experiment-3body/3D/show_3d_dataset.py
@@ -13,3 +13,13 @@ print('path =', path)
 
 data = from_pickle(path)
 print(data)
+print()
+print('coords shape:', data['coords'].shape)
+print('test_coords:', data['test_coords'].shape)
+print('dcoords shape:', data['dcoords'].shape)
+print('test_dcoords shape:', data['test_dcoords'].shape)
+print('energy shape:', data['energy'].shape)
+print('test_energy shape:', data['test_energy'].shape)
+print('meta:')
+for key, value in data['meta'].items():
+    print('\t' + key + ':', value)
